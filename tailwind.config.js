d394c3246237fabe2eaa351179e8c272df6faa0e
Dominik Dewitz <dd@thepeaklab.com>
2022-09-02 13:11:18 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './apps/drag-drop-test/pages/**/*.{js,ts,jsx,tsx}',
+    './apps/drag-drop-test/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
